884b4e964b2cf1adb46192b70427f480df8e8df4
Darius Brown <Brown.darius.r@gmail.com>
2021-05-13 14:34:13 -0500

    finished exercise

--- src/main/java/MavenTester.java
+++ src/main/java/MavenTester.java
@@ -1,7 +1,19 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class MavenTester {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello from Maven.");
+
+
+        System.out.println("type input you want to modify :)");
+        String input = sc.nextLine();
+        String modifiedInput = StringUtils.swapCase(input);
+        String reversedInput = StringUtils.reverse(modifiedInput);
+        System.out.println(modifiedInput);
+        System.out.println(reversedInput);
     }
+
 }
