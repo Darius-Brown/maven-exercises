c53f8a953fca0ccf1937458d3dafb29d6335306d
Darius Brown <Brown.darius.r@gmail.com>
2021-05-13 14:16:07 -0500

    src

---
+++ src/test/java/MavenTester.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenTester {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven.");
+    }
+
+}
